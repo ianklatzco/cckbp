1dbeada9d47837a7e23521c12bbaad7de3d6138c
Ian Klatzco <iklatzco@gmail.com>
2016-04-25 13:38:27 -0500

    add qwerty, numpad

--- tmk_keyboard-master/keyboard/bloodyfingers - left/keymap_poker.c
+++ tmk_keyboard-master/keyboard/bloodyfingers - left/keymap_poker.c
@@ -6,7 +6,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TAB, QUOT, COMM,  DOT,    P,    Y, BSLS, \
             LCTL,    A,    O,    E,    U,    I,       \
             LSFT, SCLN,    Q,    J,    K,    X,  GRV, \
-            CAPS,    0,    0, LGUI, LALT,             \
+            CAPS,  FN1,    0, LGUI, LALT,             \
                                            FN0, HOME, \
                                      SPC,  ENT,  END, \
                                                 LGUI  \
@@ -20,6 +20,17 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            TRNS, HOME, \
                                      TRNS, TRNS, TRNS, \
                                                  TRNS  \
+        ),
+    /*2: qwerty*/
+    KEYMAP(ESC,  1,   2,   3,   4,   5,   MINS, \
+           TAB,  Q,   W,   E,   R,   T,   LBRC, \
+           LCTL, A,   S,   D,   F,   G,         \
+           LSFT, Z,   X,   C,   V,   B,   BSLS, \
+           CAPS, TRNS, LGUI,F,  PAUS,           \
+                                        4,HOME, \
+                                 SPC, DEL, END, \
+                                           LGUI \
+                                                \
         ),
 };
 
@@ -39,4 +50,5 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_LAYER_TOGGLE(2), //go to keymap 2 when FN1 is pressed
 };
--- tmk_keyboard-master/keyboard/bloodyfingers - right/keymap_poker.c
+++ tmk_keyboard-master/keyboard/bloodyfingers - right/keymap_poker.c
@@ -1,20 +1,30 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    KEYMAP( RBRC,    6,    7,    8,    9,    0, RCTL, \
+    KEYMAP( RBRC,    6,    7,    8,    9,    0,  FN1, \
              EQL,    F,    G,    C,    R,    L, SLSH, \
                      D,    H,    T,    N,    S,  ENT, \
             MINS,    B,    M,    W,    V,    Z, RSFT, \
-            LEFT, DOWN,   UP, RGHT, RCTL,             \
+                        LEFT, DOWN,   UP, RGHT, RCTL, \
             PGUP,  FN0,                               \
             PGDN,  DEL, BSPC,                         \
             RGUI                                      \
         ),
     KEYMAP( TRNS,   F6,   F7,   F8,   F9,  F10,  F11, \
-            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  F12, \
-                  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
-            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
-            TRNS, TRNS, TRNS, TRNS, TRNS,             \
+            TRNS, TRNS,   P7,   P8,   P9, PMNS,  F12, \
+                  TRNS,   P4,   P5,   P6, PPLS, TRNS, \
+            TRNS, TRNS,   P1,   P2,   P3, PAST, TRNS, \
+                          P0, PDOT, PENT, PSLS, TRNS, \
+            TRNS, TRNS,                               \
+            TRNS, TRNS, TRNS,                         \
+            TRNS                                      \
+        ),
+    /*2: qwerty*/
+    KEYMAP( TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+            TRNS,    Y,    U,    I,    O,    P, TRNS, \
+                     H,    J,    K,    L, SCLN, TRNS, \
+            TRNS,    N,    M, COMM,  DOT, SLSH, TRNS, \
+                       TRNS, TRNS,  TRNS, TRNS, TRNS, \
             TRNS, TRNS,                               \
             TRNS, TRNS, TRNS,                         \
             TRNS                                      \
@@ -37,4 +47,5 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_LAYER_TOGGLE(2), //go to keymap 2 when FN1 is pressed
 };
