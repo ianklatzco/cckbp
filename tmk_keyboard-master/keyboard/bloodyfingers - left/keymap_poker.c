bbe0ede0b1470c2056a6b83735fcc0e91b19003a
Ian Klatzco <iklatzco@gmail.com>
2018-02-06 05:26:40 -0600

    remove qwerty at long last ;; FREEDOM

--- tmk_keyboard-master/keyboard/bloodyfingers - left/keymap_poker.c
+++ tmk_keyboard-master/keyboard/bloodyfingers - left/keymap_poker.c
@@ -6,7 +6,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TAB, QUOT, COMM,  DOT,    P,    Y, BSLS, \
             LCTL,    A,    O,    E,    U,    I,       \
             LSFT, SCLN,    Q,    J,    K,    X,  GRV, \
-            CAPS,  FN1,    0, LGUI, LALT,             \
+            CAPS, LBRC, RBRC, LGUI, LALT,             \
                                            FN0, HOME, \
                                      SPC,  ENT,  END, \
                                                 LGUI  \
@@ -21,34 +21,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      TRNS, TRNS, TRNS, \
                                                  TRNS  \
         ),
-    /*2: qwerty*/
-    KEYMAP(ESC,  1,   2,   3,   4,   5,   MINS, \
-           TAB,  Q,   W,   E,   R,   T,   LBRC, \
-           LCTL, A,   S,   D,   F,   G,         \
-           LSFT, Z,   X,   C,   V,   B,   BSLS, \
-           CAPS, TRNS, LGUI,F,  PAUS,           \
-                                        4,HOME, \
-                                 SPC, DEL, END, \
-                                           LGUI \
-                                                \
-        ),
 };
-
-// const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//     KEYMAP(ESC,  1,   2,   3,   4,   5,   MINS, \
-//            TAB,  Q,   W,   E,   R,   T,LBRC, \
-//            LCTL,    A,      S,     D,   F,   G,      \
-//            LSFT, Z,      X,     C,   V,   B,BSLS, \
-//            CAPS, LALT, LGUI,F,  PAUS,                \
-//                                              4,HOME, \
-//                                       SPC, DEL, END, \
-//                                                 LGUI \
-//                                                      \
-//         ),
-// };
-
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
-    [1] = ACTION_LAYER_TOGGLE(2), //go to keymap 2 when FN1 is pressed
+    // [1] = ACTION_LAYER_TOGGLE(2), //go to keymap 2 when FN1 is pressed
 };
--- tmk_keyboard-master/keyboard/bloodyfingers - right/keymap_poker.c
+++ tmk_keyboard-master/keyboard/bloodyfingers - right/keymap_poker.c
@@ -1,6 +1,7 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*0: default layer */
     KEYMAP( RBRC,    6,    7,    8,    9,    0,  FN1, \
              EQL,    F,    G,    C,    R,    L, SLSH, \
                      D,    H,    T,    N,    S,  ENT, \
@@ -10,6 +11,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             PGDN,  DEL, BSPC,                         \
             RGUI                                      \
         ),
+    /*1: FN key layer*/
     KEYMAP( TRNS,   F6,   F7,   F8,   F9,  F10,  F11, \
             TRNS, TRNS,   P7,   P8,   P9, PMNS,  F12, \
                   TRNS,   P4,   P5,   P6, PPLS, TRNS, \
@@ -19,33 +21,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             TRNS, TRNS, TRNS,                         \
             TRNS                                      \
         ),
-    /*2: qwerty*/
-    KEYMAP( TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
-            TRNS,    Y,    U,    I,    O,    P, TRNS, \
-                     H,    J,    K,    L, SCLN, TRNS, \
-            TRNS,    N,    M, COMM,  DOT, SLSH, TRNS, \
-                       TRNS, TRNS,  TRNS, TRNS, TRNS, \
-            TRNS, TRNS,                               \
-            TRNS, TRNS, TRNS,                         \
-            TRNS                                      \
-        ),
 };
-
-// const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//     KEYMAP(ESC,  1,   2,   3,   4,   5,   MINS, \
-//            TAB,  Q,   W,   E,   R,   T,LBRC, \
-//            LCTL,    A,      S,     D,   F,   G,      \
-//            LSFT, Z,      X,     C,   V,   B,BSLS, \
-//            CAPS, LALT, LGUI,F,  PAUS,                \
-//                                              4,HOME, \
-//                                       SPC, DEL, END, \
-//                                                 LGUI \
-//                                                      \
-//         ),
-// };
-
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
-    [1] = ACTION_LAYER_TOGGLE(2), //go to keymap 2 when FN1 is pressed
+    // [1] = ACTION_LAYER_TOGGLE(2), //go to keymap 2 when FN1 is pressed
 };
